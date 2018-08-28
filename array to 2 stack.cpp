7912cd70aaa4a462bf475f916178697a2e03f101
akashkhanra <khanraakash@gmail.com>
2018-08-28 15:22:09 +0530

    initial

---
+++ array to 2 stack.cpp
@@ -0,0 +1,24 @@
+//
+// Created by AKASH KHANRA on 21/08/18.
+//
+
+#include <iostream>
+#include <stack>
+using namespace std;
+int a[5],top1=0,top2=4;
+int main()
+{
+    int ch,data;
+    while(1){
+        cout<<"1. Push in 1st stack\n";
+        cout<<"2. Push in 2nd stack\n";
+        cout<<"3. POP in 1st stack\n";
+        cout<<"4. POp in 2nd stack\n";
+
+
+
+
+    }
+
+
+}
